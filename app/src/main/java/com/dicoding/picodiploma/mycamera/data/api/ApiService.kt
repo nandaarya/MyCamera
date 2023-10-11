a15db83fd817a18539f9d60c15c68a9c90b518a3
Nanda Arya Putra <nandaarya404@gmail.com>
2023-10-12 00:29:02 +0700

    add compress image size maks 1 MB

--- app/src/main/java/com/dicoding/picodiploma/mycamera/data/api/ApiService.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/data/api/ApiService.kt
@@ -2,7 +2,6 @@ package com.dicoding.picodiploma.mycamera.data.api
 
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
-import retrofit2.Call
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
