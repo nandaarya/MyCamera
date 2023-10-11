db7a662b9688939e0e3ca41cb09b6e6d913e9ac0
Nanda Arya Putra <nandaarya404@gmail.com>
2023-10-12 00:00:12 +0700

    add retrofit configuration for upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,9 @@ dependencies {
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
     implementation("androidx.camera:camera-view:$cameraxVersion")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1") //untuk lifecycleScope
 }
